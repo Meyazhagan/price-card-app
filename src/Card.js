ff011944134373ec1931017174571d53a54925f8
Meyazhagan <meyazhagan.ofcl@gmail.com>
2021-09-09 14:59:59 +0530

    changed

--- src/Card.js
+++ src/Card.js
@@ -27,7 +27,7 @@ class Card extends Component {
                       <span className="fa-li">
                         <i className="fas fa-check"></i>
                       </span>
-                      {title}
+                      <h1>{title}</h1>
                     </li>
                   );
                 } else {
